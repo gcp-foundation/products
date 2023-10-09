ab6917eb993bfcc4ffc2151f83db8f7e61366a07
Dave Butland <dgbutland@gmail.com>
2023-10-09 19:30:21 +0100

    Added management iam policy

--- devops/control.tf
+++ devops/control.tf
@@ -147,14 +147,22 @@ locals {
   devops_env = {
     organization_id = module.organization.org_id
     devops_sa       = "serviceAccount:${module.service_account["devops"].email}"
+    mananagement_sa = "serviceAccount:${module.service_account["management"].email}"
   }
 
-  devops_policy = yamldecode(templatefile("${path.module}/devops_policy.yaml", local.devops_env))
+  devops_policy     = yamldecode(templatefile("${path.module}/devops_policy.yaml", local.devops_env))
+  management_policy = yamldecode(templatefile("${path.module}/management_policy.yaml", local.devops_env))
 }
 
 module "devops_iam" {
   source = "github.com/gcp-foundation/modules//iam/policy?ref=0.0.1"
 
   policy = local.devops_policy
+}
+
+module "management_iam" {
+  source = "github.com/gcp-foundation/modules//iam/policy?ref=0.0.1"
+
+  policy = local.management_policy
 }
 
