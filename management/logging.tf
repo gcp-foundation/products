eb265f44a59c1cda56739fe80a1545a70ea5bc7e
Dave Butland <dgbutland@gmail.com>
2023-10-10 11:57:08 +0100

    If this fails, I'm confused

--- management/logging.tf
+++ management/logging.tf
@@ -11,8 +11,8 @@ locals {
   other_encrypters = [
     "serviceAccount:${data.google_storage_project_service_account.logging_gcs_account.email_address}",
     # "serviceAccount:bq-${local.projects["logging"].number}@bigquery-encryption.iam.gserviceaccount.com}"
-    "serviceAccount:bq-${local.projects["logging"].number}@bigquery-encryption.iam.gserviceaccount.com",
-    "serviceAccount:bq-164725902861@bigquery-encryption.iam.gserviceaccount.com"
+    "serviceAccount:bq-${local.projects["logging"].number}@bigquery-encryption.iam.gserviceaccount.com"
+    # "serviceAccount:bq-164725902861@bigquery-encryption.iam.gserviceaccount.com"
   ]
   logging_encrypters = concat([for identity in module.logging_service_identity : "serviceAccount:${identity.email}"], local.other_encrypters)
 }
