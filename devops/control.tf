83943d23507f48b1f7a99b48f7b145697dec7d49
Dave Butland <dgbutland@gmail.com>
2023-10-10 13:04:21 +0100

    Added iam permissions back

--- devops/control.tf
+++ devops/control.tf
@@ -143,26 +143,26 @@ resource "google_storage_bucket_iam_member" "sa_service_account_billing_admin" {
 }
 */
 
-# locals {
-#   devops_env = {
-#     organization_id = module.organization.org_id
-#     devops_sa       = "serviceAccount:${module.service_account["devops"].email}"
-#     management_sa   = "serviceAccount:${module.service_account["management"].email}"
-#   }
+locals {
+  devops_env = {
+    organization_id = module.organization.org_id
+    devops_sa       = "serviceAccount:${module.service_account["devops"].email}"
+    management_sa   = "serviceAccount:${module.service_account["management"].email}"
+  }
 
-#   devops_policy     = yamldecode(templatefile("${path.module}/devops_policy.yaml", local.devops_env))
-#   management_policy = yamldecode(templatefile("${path.module}/management_policy.yaml", local.devops_env))
-# }
+  devops_policy     = yamldecode(templatefile("${path.module}/devops_policy.yaml", local.devops_env))
+  management_policy = yamldecode(templatefile("${path.module}/management_policy.yaml", local.devops_env))
+}
 
-# module "devops_iam" {
-#   source = "github.com/gcp-foundation/modules//iam/policy?ref=0.0.1"
+module "devops_iam" {
+  source = "github.com/gcp-foundation/modules//iam/policy?ref=0.0.1"
 
-#   policy = local.devops_policy
-# }
+  policy = local.devops_policy
+}
 
-# module "management_iam" {
-#   source = "github.com/gcp-foundation/modules//iam/policy?ref=0.0.1"
+module "management_iam" {
+  source = "github.com/gcp-foundation/modules//iam/policy?ref=0.0.1"
 
-#   policy = local.management_policy
-# }
+  policy = local.management_policy
+}
 
