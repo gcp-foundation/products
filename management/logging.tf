8158166112a82fb3eb9268db30c5f74e8a075f0c
Dave Butland <dgbutland@gmail.com>
2023-10-10 11:09:10 +0100

    Added project number

--- management/logging.tf
+++ management/logging.tf
@@ -7,10 +7,10 @@ locals {
     logName: /logs/cloudaudit.googleapis.com%2Faccess_transparency
   EOF
 
-  logging_services = ["pubsub.googleapis.com", "artifactregistry.googleapis.com", "bigquery.googleapis.com"]
+  logging_services = ["pubsub.googleapis.com", "artifactregistry.googleapis.com"]
   other_encrypters = [
     "serviceAccount:${data.google_storage_project_service_account.logging_gcs_account.email_address}"
-    #    "serviceAccount:bq-${local.projects.number}@bigquery-encryption.iam.gserviceaccount.com}"
+    "serviceAccount:bq-${local.projects["logging"].number}@bigquery-encryption.iam.gserviceaccount.com}"
   ]
   logging_encrypters = concat([for identity in module.logging_service_identity : "serviceAccount:${identity.email}"], local.other_encrypters)
 }
@@ -18,19 +18,19 @@ locals {
 module "logging_service_identity" {
   source   = "github.com/gcp-foundation/modules//resources/service_identity?ref=0.0.1"
   for_each = toset(local.logging_services)
-  project  = local.projects["logging"]
+  project  = local.projects["logging"].project_id
   service  = each.value
 }
 
 data "google_storage_project_service_account" "logging_gcs_account" {
-  project = local.projects["logging"]
+  project = local.projects["logging"].project_id
 }
 
 module "logging_kms_key" {
   source        = "github.com/gcp-foundation/modules//kms/key?ref=0.0.1"
-  name          = local.projects["logging"]
-  key_ring_name = local.projects["logging"]
-  project       = local.projects["logging"]
+  name          = local.projects["logging"].project_id
+  key_ring_name = local.projects["logging"].project_id
+  project       = local.projects["logging"].project_id
   location      = var.location
   encrypters    = local.logging_encrypters
   decrypters    = local.logging_encrypters
@@ -49,7 +49,7 @@ module "log_sink_all_to_storage" {
 module "log_storage" {
   source              = "github.com/gcp-foundation/modules//storage/bucket?ref=0.0.1"
   name                = "log_storage"
-  project             = local.projects["logging"]
+  project             = local.projects["logging"].project_id
   location            = var.location
   kms_key_id          = module.logging_kms_key.key_id
   data_classification = "logs"
@@ -68,14 +68,14 @@ module "log_sink_filtered_to_bigquery" {
   name             = "log_bigquery"
   org_id           = local.organization_id
   include_children = true
-  destination      = "bigquery.googleapis.com/projects/${local.projects["logging"]}/datasets/${module.log_bigquery.dataset_id}"
+  destination      = "bigquery.googleapis.com/projects/${local.projects["logging"].project_id}/datasets/${module.log_bigquery.dataset_id}"
   filter           = local.log_filter
 }
 
 module "log_bigquery" {
   source     = "github.com/gcp-foundation/modules//bigquery/dataset?ref=0.0.1"
   name       = "log_bigquery"
-  project    = local.projects["logging"]
+  project    = local.projects["logging"].project_id
   location   = var.location
   kms_key_id = module.logging_kms_key.key_id
 
@@ -83,7 +83,7 @@ module "log_bigquery" {
 }
 
 resource "google_project_iam_member" "bigquery_sink_member" {
-  project = local.projects["logging"]
+  project = local.projects["logging"].project_id
   role    = "roles/bigquery.dataEditor"
   member  = module.log_sink_filtered_to_bigquery.writer_identity
 }
--- management/resources.tf
+++ management/resources.tf
@@ -30,7 +30,8 @@ locals {
   }
 
   projects = {
-    for project in data.google_cloud_asset_resources_search_all.projects.results : project.display_name => substr(project.name, 47, -1)
+    for project in data.google_cloud_asset_resources_search_all.projects.results :
+    project.display_name => { project_id = substr(project.name, 47, -1), number = substr(project.project, 9, -1) }
   }
 }
 
