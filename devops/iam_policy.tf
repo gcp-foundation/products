4f3cd1fb763253e2272ab90c56af211093a073b2
SudharsaneSivamany <sudharsanes694@gmail.com>
2023-10-14 03:52:44 +0530

    added

--- devops/iam_policy.tf
+++ devops/iam_policy.tf
@@ -2,12 +2,11 @@ locals {
   organization_bindings = flatten([
     for organization in local.iam_policy.organizations : [
       for binding in organization.iamPolicy.bindings : [
-        for members in binding.members : [
-          for member in members : {
-            org_id = organization.name
-            role   = binding.role
-            member = member
-        }]
+        for members in binding.members : {
+          org_id = organization.name
+          role   = binding.role
+          member = member
+        }
       ]
     ]
   ])
@@ -16,12 +15,11 @@ locals {
   folder_bindings = flatten([
     for folder in local.iam_policy.folders : [
       for binding in folder.iamPolicy.bindings : [
-        for member in binding.members : [
-          for member in members : {
-            folder_id = folder.name
-            role      = binding.role
-            member    = member
-        }]
+        for member in binding.members : {
+          folder_id = folder.name
+          role      = binding.role
+          member    = member
+        }
       ]
     ]
   ])
@@ -30,19 +28,18 @@ locals {
   project_bindings = flatten([
     for project in local.iam_policy.projects : [
       for binding in project.iamPolicy.bindings : [
-        for member in binding.members : [
-          for member in members : {
-            project_id = project.name
-            role       = binding.role
-            member     = member
-        }]
+        for member in binding.members : {
+          project_id = project.name
+          role       = binding.role
+          member     = member
+        }
       ]
     ]
   ])
 }
 
 resource "google_organization_iam_member" "organization" {
-  for_each = { for binding in local.organization_bindings : "${binding.org_id}/${binding.role}/${binding.member}" => binding }
+  for_each = toset(flatten([for binding in local.organization_bindings : { for member in binding.member : "${binding.org_id}/${binding.role}/${member.member}" => binding }]))
 
   org_id = module.organization.org_id
   role   = each.value.role
@@ -50,7 +47,7 @@ resource "google_organization_iam_member" "organization" {
 }
 
 resource "google_folder_iam_member" "folder" {
-  for_each = { for binding in local.folder_bindings : "${binding.folder_id}/${binding.role}/${binding.member}" => binding }
+  for_each = toset(flatten([for binding in local.folder_bindings : { for member in binding.member : "${binding.folder_id}/${binding.role}/${member.member}" => binding }]))
 
   folder = module.folders[each.value.folder_id].id
   role   = each.value.role
@@ -58,7 +55,7 @@ resource "google_folder_iam_member" "folder" {
 }
 
 resource "google_project_iam_member" "project" {
-  for_each = { for binding in local.project_bindings : "${binding.project_id}/${binding.role}/${binding.member}" => binding }
+  for_each = toset(flatten([for binding in local.project_bindings : { for member in binding.member : "${binding.project_id}/${binding.role}/${member.member}" => binding }]))
 
   project = module.projects[each.value.project_id].project
   role    = each.value.role
