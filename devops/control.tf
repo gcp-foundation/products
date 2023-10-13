f68ba6f9d548a0fa8a8cf24b702cc6cb239fa234
Dave Butland <dgbutland@gmail.com>
2023-10-13 17:01:48 +0100

    Added projects iam definition

--- devops/control.tf
+++ devops/control.tf
@@ -143,27 +143,3 @@ resource "google_storage_bucket_iam_member" "sa_service_account_billing_admin" {
   member             = "serviceAccount:${module.service_account[each.key].email}"
 }
 */
-
-locals {
-  devops_env = {
-    organization_id = module.organization.org_id
-    devops_sa       = "serviceAccount:${module.service_account["devops"].email}"
-    management_sa   = "serviceAccount:${module.service_account["management"].email}"
-  }
-
-  devops_policy     = yamldecode(templatefile("${path.module}/devops_policy.yaml", local.devops_env))
-  management_policy = yamldecode(templatefile("${path.module}/management_policy.yaml", local.devops_env))
-}
-
-module "devops_iam" {
-  source = "github.com/gcp-foundation/modules//iam/policy?ref=0.0.1"
-
-  policy = local.devops_policy
-}
-
-module "management_iam" {
-  source = "github.com/gcp-foundation/modules//iam/policy?ref=0.0.1"
-
-  policy = local.management_policy
-}
-
