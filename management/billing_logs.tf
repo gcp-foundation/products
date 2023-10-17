5cae2cb3309911c8972d4d37fca6984276e9c14c
SudharsaneSivamany <sudharsanes694@gmail.com>
2023-10-17 21:09:49 +0530

    added delete_contents_on_destroy for bigquerry dataset module

--- management/billing_logs.tf
+++ management/billing_logs.tf
@@ -1,20 +1,23 @@
 module "billing_dataset" {
-  source   = "github.com/XBankGCPOrg/gcp-lz-modules//bigquery/dataset?ref=v0.0.1"
-  name     = "billing"
-  project  = local.projects[local.environment.project_billing].project_id
-  location = var.location
+  source                     = "github.com/XBankGCPOrg/gcp-lz-modules//bigquery/dataset?ref=v0.0.1"
+  name                       = "billing"
+  delete_contents_on_destroy = true
+  project                    = local.projects[local.environment.project_billing].project_id
+  location                   = var.location
 }
 
 module "billing_details_dataset" {
-  source   = "github.com/XBankGCPOrg/gcp-lz-modules//bigquery/dataset?ref=v0.0.1"
-  name     = "billing_details"
-  project  = local.projects[local.environment.project_billing].project_id
-  location = var.location
+  source                     = "github.com/XBankGCPOrg/gcp-lz-modules//bigquery/dataset?ref=v0.0.1"
+  name                       = "billing_details"
+  delete_contents_on_destroy = true
+  project                    = local.projects[local.environment.project_billing].project_id
+  location                   = var.location
 }
 
 module "pricing_dataset" {
-  source   = "github.com/XBankGCPOrg/gcp-lz-modules//bigquery/dataset?ref=v0.0.1"
-  name     = "pricing"
-  project  = local.projects[local.environment.project_billing].project_id
-  location = var.location
+  source                     = "github.com/XBankGCPOrg/gcp-lz-modules//bigquery/dataset?ref=v0.0.1"
+  name                       = "pricing"
+  delete_contents_on_destroy = true
+  project                    = local.projects[local.environment.project_billing].project_id
+  location                   = var.location
 }
