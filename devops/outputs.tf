0364d7d898c6e26fbe4ca20a576ce9b09d034a2a
SudharsaneSivamany <sudharsanes694@gmail.com>
2023-10-17 13:51:04 +0530

    added output for wif

--- devops/outputs.tf
+++ devops/outputs.tf
@@ -2,6 +2,6 @@ output "gcs-bucket-name" {
   value = module.state_files.name
 }
 
-output "service-account" {
-  value = [for sa, des in local.service_accounts : module.service_account[sa].email]
+output "workload_identity_provider" {
+  value = [for sa, des in var.authorized_repositories : { service_account = module.service_account[sa].email, workload_identity_provider = "projects/${module.projects["devops/control"].number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.github[sa].id}/providers/${google_iam_workload_identity_pool_provider.github[sa].id}" }]
 }
