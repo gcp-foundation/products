6429a3c0f8432e769ce7c37768860e9841997a4a
SudharsaneSivamany <sudharsanes694@gmail.com>
2023-10-19 14:43:13 +0530

    issue

--- devops/main.tf
+++ devops/main.tf
@@ -5,18 +5,18 @@ module "organization" {
 
 module "folders" {
   source      = "github.com/XBankGCPOrg/gcp-lz-modules//resources/folder?ref=osttra-release"
-  folder_list = var.organization.folder
+  folder_list = var.organization.folders
   parent_name = module.organization.name
 }
 
 module "projects" {
   source   = "github.com/XBankGCPOrg/gcp-lz-modules//resources/project?ref=v0.0.1"
-  for_each = { for entry in var.organization.projects : entry.project.displayName => entry }
+  for_each = { for entry in var.organization.projects : entry.displayName => entry }
 
   name            = each.value.displayName
   folder          = flatten([for folder in module.folders.folder_id : values(folder) if contains(keys(folder), each.value.parent)]).0
   services        = each.value.services
-  billing_account = try(each.value.project.billingAccount, var.billing_account)
+  billing_account = try(each.value.billingAccount, var.billing_account)
   labels          = var.labels
 }
 
