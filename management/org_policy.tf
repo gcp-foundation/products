9bc9a3b8fab08c09814dc721ba6e5915c813fab7
Dave Butland <dgbutland@gmail.com>
2023-11-01 12:50:39 +0000

    Added organization to org policy file

--- management/org_policy.tf
+++ management/org_policy.tf
@@ -1,31 +1,34 @@
 locals {
 
-  organization = yamldecode(templatefile("${path.module}/org_policy.yaml", local.environment))
+  org_policies = yamldecode(templatefile("${path.module}/org_policy.yaml", local.environment))
 
-  organization_policies = flatten([
-    for policy in local.organization.orgPolicy :
-    { policy = policy } if try(policy.exists, false) != true
-  ])
+  organization_policies = {
+    for organization in local.org_policies.organizations : organization.displayName => [
+      for policy in organization.orgPolicy :
+      { policy = policy } if try(policy.exists, false) != true
+  ] }
 
   folder_policies = {
-    for folder in local.organization.folders : "organizations/${local.organization_id}/${folder.displayName}" => [
+    for folder in local.org_policies.folders : "organizations/${local.organization_id}/${folder.displayName}" => [
       for policy in folder.orgPolicy :
       { policy = policy } if try(policy.exists, false) != true
     ]
   }
 
   project_policies = {
-    for project in local.organization.projects : project.displayName => [
+    for project in local.org_policies.projects : project.displayName => [
       for policy in project.orgPolicy :
       { policy = policy } if try(policy.exists, false) != true
     ]
   }
 }
 
+// Note: doesn't support multiple organizations
 module "organization_policy" {
   source   = "github.com/gcp-foundation/modules//iam/org_policy?ref=0.0.2"
+  for_each = local.organization_policies
   parent   = "organizations/${local.organization_id}"
-  policies = local.organization_policies
+  policies = each.value
 }
 
 module "folder_policies" {
@@ -40,4 +43,9 @@ module "project_policies" {
   for_each = local.project_policies
   parent   = "projects/${local.projects[each.key].project_id}"
   policies = each.value
+}
+
+moved {
+  from = module.organization_policy
+  to   = module.organization_policy["latest.gcp-foundation.com"]
 }
